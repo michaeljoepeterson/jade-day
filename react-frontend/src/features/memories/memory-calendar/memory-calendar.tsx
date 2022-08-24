ad1fd058dc8787404372f7edf717f26434d1f8d1
michael peterson <michaeljoepeterson@gmail.com>
2022-08-23 20:36:58 -0600

    first working calendar with date select

--- react-frontend/src/features/memories/memory-calendar/memory-calendar.tsx
+++ react-frontend/src/features/memories/memory-calendar/memory-calendar.tsx
@@ -1,17 +1,34 @@
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from '@fullcalendar/daygrid' 
+import React, { useEffect } from "react";
 
 const MemoryCalendar = ({
-    height = '45em'
+    height = '45em',
+    startDate = new Date()
 }:{
     height?: string;
+    startDate?: Date
 }) => {
+    const calendar = React.createRef<FullCalendar>();
+
+    useEffect(() => {
+        try{
+            const api = calendar?.current?.getApi();
+            api?.gotoDate(startDate);
+        }
+        catch(e){
+            console.warn(e);
+        }
+    }, [startDate])
+
     return(
         <div className="h-full">
             <FullCalendar
+            ref={calendar}
             height={height}
             plugins={[ dayGridPlugin ]}
-            initialView="dayGridMonth"/>
+            initialView="dayGridMonth"
+            initialDate={startDate}/>
         </div>
     )
 }
--- react-frontend/src/pages/create-memory-page.tsx
+++ react-frontend/src/pages/create-memory-page.tsx
@@ -1,11 +1,20 @@
+import { useState } from "react"
 import MemoryCalendar from "../features/memories/memory-calendar/memory-calendar"
 import withLoggedIn from "../HOC/withLoggedIn"
 
 const CreateMemoryPage = () => {
+    const [selectedDate, setSelectedDate] = useState<Date>(new Date());
+
+    const updateDate = () => {
+        setSelectedDate(new Date('2012/12/12'));
+        console.log('update date');
+    }
+
     return(
         <div>
-            <h4>Create a Memory</h4>
-            <MemoryCalendar/>
+            <h4 onClick={(e) => updateDate()}>Create a Memory</h4>
+            <MemoryCalendar 
+            startDate={selectedDate}/>
         </div>
     )
 }

460c5c9f364e41c5c1fb6ef5a821ae9bd3372315
michael peterson <michaeljoepeterson@gmail.com>
2022-09-07 18:46:16 -0600

    first working calendar selector

--- react-frontend/src/features/memories/components/memory-calendar/calendar-controls.tsx
+++ react-frontend/src/features/memories/components/memory-calendar/calendar-controls.tsx
@@ -6,31 +6,42 @@ import { useCallback, useEffect, useState } from "react";
 
 
 const CalendarControls = ({
-    selectedDate
+    selectedDate,
+    onDateChange
 }: {
-    onDateChange?: () => void;
+    onDateChange: (date: Date) => void;
     selectedDate?: Date | null;
 }) => {
+    const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
+    ];
     const [value, setValue] = useState<Moment>(moment(selectedDate));
 
     useEffect(() => {
-        console.log(selectedDate);
         setValue(moment(selectedDate ? selectedDate : new Date()));
     }, [selectedDate]);
 
-    const handleDateChange = useCallback((val: any) => {
+    const handleDateChange = useCallback((val: Moment | null) => {
+        if(!val){
+            return;
+        }
         setValue(val);
+        onDateChange(val.toDate());
     }, []);
     
     return (
         <LocalizationProvider dateAdapter={AdapterMoment}>
-            <MobileDatePicker
-                label="Select a Date"
-                inputFormat="MM/DD/YYYY"
-                value={value}
-                onChange={handleDateChange}
-                renderInput={(params) => <TextField {...params} />}
-            />
+            <div className="flex justify-between items-center mb-4">
+                <h4 className="text-xl">
+                    {monthNames[value.month()]}
+                </h4>
+                <MobileDatePicker
+                    label="Select a Date"
+                    inputFormat="MM/DD/YYYY"
+                    value={value}
+                    onChange={handleDateChange}
+                    renderInput={(params) => <TextField {...params} />}
+                />
+            </div>
         </LocalizationProvider>
     );
 }

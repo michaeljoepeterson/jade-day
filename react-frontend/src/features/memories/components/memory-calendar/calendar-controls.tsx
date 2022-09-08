cef2159efb7b0e667c1835b17b15102943263287
michael peterson <michaeljoepeterson@gmail.com>
2022-09-07 19:43:06 -0600

    first working highlight dates with created memories

--- react-frontend/src/features/memories/components/memory-calendar/calendar-controls.tsx
+++ react-frontend/src/features/memories/components/memory-calendar/calendar-controls.tsx
@@ -1,24 +1,38 @@
 import { TextField } from "@mui/material";
-import { LocalizationProvider, MobileDatePicker } from "@mui/x-date-pickers";
+import { LocalizationProvider, MobileDatePicker, PickersDay, PickersDayProps } from "@mui/x-date-pickers";
 import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
 import moment, { Moment } from "moment";
 import { useCallback, useEffect, useState } from "react";
+import { IMemory } from "../../../../models/memories/memory";
 
 
 const CalendarControls = ({
     selectedDate,
-    onDateChange
+    onDateChange,
+    memories = []
 }: {
     onDateChange: (date: Date) => void;
     selectedDate?: Date | null;
+    memories?: IMemory[];
 }) => {
     const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
     ];
     const [value, setValue] = useState<Moment>(moment(selectedDate));
+    const [memoryLookup, setMemoryLookup] = useState<Map<string, IMemory>>(new Map);
 
     useEffect(() => {
         setValue(moment(selectedDate ? selectedDate : new Date()));
     }, [selectedDate]);
+
+    useEffect(() => {
+        const lookup = new Map();
+        memories.forEach(memory => {
+            if(memory.date){
+                lookup.set(memory.date.toDateString(), memory);
+            }
+        })
+        setMemoryLookup(lookup);
+    }, [memories]);
 
     const handleDateChange = useCallback((val: Moment | null) => {
         if(!val){
@@ -27,6 +41,23 @@ const CalendarControls = ({
         setValue(val);
         onDateChange(val.toDate());
     }, []);
+
+    const renderDay = (day: Moment, selected: Moment[], props: PickersDayProps<Moment>) => {
+        const dateString = day.toDate().toDateString();
+        if(memoryLookup.has(dateString)){
+            return (
+                <PickersDay 
+                    className="bg-primary text-white"
+                    {...props}
+                />
+            );
+        }
+        return (
+            <PickersDay
+                {...props}
+            />
+        );
+    }
     
     return (
         <LocalizationProvider dateAdapter={AdapterMoment}>
@@ -35,6 +66,7 @@ const CalendarControls = ({
                     {monthNames[value.month()]}
                 </h4>
                 <MobileDatePicker
+                    renderDay={renderDay}
                     label="Select a Date"
                     inputFormat="MM/DD/YYYY"
                     value={value}
--- react-frontend/src/features/memories/components/memory-calendar/memory-calendar.tsx
+++ react-frontend/src/features/memories/components/memory-calendar/memory-calendar.tsx
@@ -69,6 +69,7 @@ const MemoryCalendar = ({
             <div>
                 <CalendarControls
                     onDateChange={(date) => dateChanged(date)}
+                    memories={memories}
                 />
             </div>
             <FullCalendar
